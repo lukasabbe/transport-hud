8c003733825b29773fe78ac8401c98cf1e1bae01
Lukas <67807954+lukasabbe@users.noreply.github.com>
2024-09-17 12:39:00 +0200

    Weird merge forgot to change file

--- src/main/java/me/lukasabbe/transporthud/huds/ElytraHUD.java
+++ src/main/java/me/lukasabbe/transporthud/huds/ElytraHUD.java
@@ -112,11 +112,7 @@ public class ElytraHUD implements HudRenderCallback {
             int y = MathHelper.lerp(
                     MathHelper.map(i,0,points,0,1),
                     posY,pos2y);
-<<<<<<< 1.21.2
             context.drawTexture(RenderLayer::getGuiTextured,elytraHudAssets,x,y,77,12,1,1,1,1,256,256);
-=======
-            context.drawTexture(elytraHudAssets,x,y,77,12,1,1);
->>>>>>> main
         }
     }
 
